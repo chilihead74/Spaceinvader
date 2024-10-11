af2d2579dfb5356146bcf9c31a03b8dde64e33c9
rb-mwinzinger <84082663+rb-mwinzinger@users.noreply.github.com>
2024-10-11 11:02:34 +0000

    add missing tags

--- Michael/main.tf
+++ Michael/main.tf
@@ -1,4 +1,9 @@
 resource "azurerm_resource_group" "light_yagami" {
   name     = "SpaceInvader-LightYagami"
   location = "West Europe"
+   tags     = {
+        "environment"    = "sandbox"
+        "internal-order" = "INTOUTCLDINF"
+        "owner"          = "dmitrii.tyryshkin@redbull.com"
+    }
 }
