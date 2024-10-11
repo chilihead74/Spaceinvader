7c058d0345498a3218527748ec3b33a5e61030e2
peuxn <christian.peuker@sbg.at>
2024-10-11 11:47:44 +0000

    fix

--- Chris/profile.md
+++ Chris/profile.md
@@ -1,4 +1,4 @@
-resource "spaceship_crew_member" "elena_kim" {
+resource "spaceship_crew_member" "Christian" {
   name        = "Christian"
   experience  = "15 years"
   specialty   = "Space Medicine"
---
+++ main.tf
@@ -0,0 +1,13 @@
+# Create a resource group
+resource "azurerm_resource_group" "example" {
+  name     = "rg-christian"
+  location = "West Europe"
+}
+
+# Create a virtual network within the resource group
+resource "azurerm_virtual_network" "example" {
+  name                = "net-christian"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  address_space       = ["10.0.0.0/16"]
+}
