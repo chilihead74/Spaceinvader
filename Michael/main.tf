ad7ce4cdc30872d60e4e00ee438afa084ea6cecf
rb-mwinzinger <84082663+rb-mwinzinger@users.noreply.github.com>
2024-10-18 09:32:55 +0000

    change region and add vnet

--- Michael/main.tf
+++ Michael/main.tf
@@ -1,7 +1,24 @@
 resource "azurerm_resource_group" "light_yagami" {
   name     = "SpaceInvader-LightYagami"
-  location = "West Europe"
-   tags     = {
-        "owner"          = "michael.winzinger@redbull.com"
-    }
+  location = "swedencentral"
+  tags = {
+    "owner" = "michael.winzinger@redbull.com"
+  }
+}
+
+resource "azurerm_virtual_network" "vnet01" {
+  address_space       = ["10.0.0.0/16"]
+  name                = "vnet01"
+  location            = azurerm_resource_group.light_yagami.location
+  resource_group_name = azurerm_resource_group.light_yagami.name
+  tags = {
+    "owner" = "michael.winzinger@redbull.com"
+  }
+}
+
+resource "azurerm_subnet" "subnet01" {
+  address_prefixes     = ["10.0.0.0/16"]
+  name                 = "subnet01"
+  virtual_network_name = azurerm_virtual_network.vnet01.name
+  resource_group_name  = azurerm_resource_group.light_yagami.name
 }
