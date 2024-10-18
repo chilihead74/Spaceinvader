36a737e1c509d7dd8a8690077369d846922e8e6f
peuxn <christian.peuker@sbg.at>
2024-10-18 08:58:22 +0000

    Challenge 1

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@
 resource "azurerm_resource_group" "example" {
   name     = "rg-christian"
   location = "West Europe"
+   tags = {"owner" = "christian.peuker@redbull.com"}
 }
 
 # Create a virtual network within the resource group
@@ -10,4 +11,5 @@ resource "azurerm_virtual_network" "example" {
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
   address_space       = ["10.0.0.0/16"]
+  tags = {"owner" = "christian.peuker@redbull.com"}
 }
