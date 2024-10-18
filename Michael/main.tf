f2cc2cda11dbba4d9d65e4e1347bbb0394ed6b14
rb-mwinzinger <84082663+rb-mwinzinger@users.noreply.github.com>
2024-10-18 08:58:44 +0000

    update tags on resource group

--- Michael/main.tf
+++ Michael/main.tf
@@ -2,8 +2,6 @@ resource "azurerm_resource_group" "light_yagami" {
   name     = "SpaceInvader-LightYagami"
   location = "West Europe"
    tags     = {
-        "environment"    = "sandbox"
-        "internal-order" = "INTOUTCLDINF"
-        "owner"          = "dmitrii.tyryshkin@redbull.com"
+        "owner"          = "michael.winzinger@redbull.com"
     }
 }
