fc67107771d69a16a5b8e88a0018bc7048665df7
chilihead74 <160751038+chilihead74@users.noreply.github.com>
2024-10-18 10:03:11 +0000

    add subnet

--- Rob/main.tf
+++ Rob/main.tf
@@ -14,3 +14,9 @@ resource "azurerm_virtual_network" "spacenetwork" {
   location            = azurerm_resource_group.robrg.location
   address_space       = ["10.0.0.0/16"]
 }
+resource "azurerm_subnet" "example" {
+  name                 = "example-subnet"
+  resource_group_name  = azurerm_resource_group.robrg.name
+  virtual_network_name = azurerm_virtual_network.spacenetwork.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
